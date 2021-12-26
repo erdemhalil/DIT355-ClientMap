752223a0192eeac9feb8b8e2eb59f4f24d2526ed
Cuong Darma <darma@student.chalmers.se>
2021-12-26 20:26:32 +0100

    rewritten with react, websocket pending

---
+++ Dentistimo/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Map from "./map";
+// import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+
+function App() {
+  return (
+    <div className="App" style={{height: "100%"}}>
+      <Map />
+    </div>
+  );
+}
+
+export default App;
---
+++ Dentistimo/src/map.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+export default function Map() {
+    
+    return (
+        <div id="map-container">
+            <div id="locations-panel">
+                <div id="locations-panel-list">
+                <header>
+                    <h1 className="search-title">
+                    <img src="https://fonts.gstatic.com/s/i/googlematerialicons/place/v15/24px.svg"/>
+                    Enter your location to find the nearest Dentist
+                    </h1>
+                    <div className="search-input">
+                    <input id="location-search-input" placeholder="Enter your address or zip code" />
+                    <div id="search-overlay-search" className="search-input-overlay search">
+                        <button id="location-search-button">
+                        <img className="icon" src="https://fonts.gstatic.com/s/i/googlematerialicons/search/v11/24px.svg" alt="Search"/>
+                        </button>
+                    </div>
+                    </div>
+                </header>
+                <div className="section-name" id="location-results-section-name">
+                    All locations
+                </div>
+                <div className="results">
+                    <ul id="location-results-list"></ul>
+                </div>
+                </div>
+                <div id="locations-panel-details"></div>
+            </div>
+            <div id="map"></div>
+        </div>
+    )
+}
+
